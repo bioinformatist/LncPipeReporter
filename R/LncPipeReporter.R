a01cbd320c1803cf66d6c244c011ca2d74cbd836
bioinformatist <sun_yu@mail.nankai.edu.cn>
2017-10-08 22:13:55 +0800

    package help page added

---
+++ R/LncPipeReporter.R
@@ -0,0 +1,11 @@
+#' LncPipeReporter: Automatically Aggregating and Summarizing Bioinformatics Results for Interactive Report.
+#'
+#' The LncPipeReporter package provides three categories of important functions:
+#' foo, bar and baz.
+#' 
+#' @section LncPipeReporter functions:
+#' The foo functions ...
+#'
+#' @docType package
+#' @name LncPipeReporter
+NULL
