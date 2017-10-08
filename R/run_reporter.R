16bb2118cd130f8976b12cb929be340dea5346b7
bioinformatist <sun_yu@mail.nankai.edu.cn>
2017-10-08 18:06:36 +0800

    doc updated

--- R/run_reporter.R
+++ R/run_reporter.R
@@ -1,3 +1,26 @@
+#' Run the main reporting pipeline
+#'
+#' @param input Absolute path of input directory (results of up-stream analysis).
+#' @param output Output file name (In HTML format).
+#' @param theme Journal palette applied to all plots supplied by package [ggsci](../../ggsci/doc/ggsci.html).
+#' @param cdf.percent Percentage of values to display when calculating coding potential.
+#' @param max.lncrna.len Maximum length of lncRNAs to display when calculating distribution (percentage).
+#' @param min.expressed.sample Minimal percentage of expressed samples (percentage).
+#'
+#' @return None
+#' @export
+#' 
+#' @details The function call a R markdown script internally by `rmarkdown::render`. 
+#' There're also default paramter values in the script -- sometimes if the user try run it manually in *RStudio*,
+#' *knitr* will bring him/her a graphical interface to choose/fill the value of paramters. 
+#' Also you can use statement like `rmarkdown::render('./reporter.Rmd', params = 'ask')` to call the interface in browser.
+#' 
+#' @examples
+#' run_reporter(input = system.file(paste0("extdata", .Platform$file.sep, "demo_results"),
+#'              package = "LncPipeReporter"),
+#'              output = 'reporter.html', theme = 'npg', cdf.percent = 10,
+#'              max.lncrna.len = 10000, min.expressed.sample = 50)
+#' 
 run_reporter <- function(input = system.file(paste0("extdata", .Platform$file.sep, "demo_results"),
                                              package = "LncPipeReporter"),
                          output = 'reporter.html',
