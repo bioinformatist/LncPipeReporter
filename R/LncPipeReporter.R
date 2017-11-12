80217c17e17318101da2a8d9fe40f61067a115b0
bioinformatist <sun_yu@mail.nankai.edu.cn>
2017-11-12 23:23:35 +0800

    fix imports

--- R/LncPipeReporter.R
+++ R/LncPipeReporter.R
@@ -8,7 +8,7 @@
 #'
 #' @docType package
 #' @name LncPipeReporter
-#' @importFrom plotly ggplotly "%>%" layout plot_ly
+#' @importFrom plotly ggplotly "%>%" layout plot_ly add_pie
 #' @importFrom cowplot save_plot
 #' @import ggplot2 ggsci edgeR data.table
 NULL
